f5e30674414183459fc62599230a426ce79c2e87
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2026-01-16 03:22:32 +0700

    Menambahkan model Crew untuk menyimpan data kru kapal

---
+++ app/src/main/java/com/example/mykapal/data/model/Crew.kt
@@ -0,0 +1,13 @@
+package com.example.mykapal.data.model
+
+data class Crew(
+    val crew_id: Int,
+    val nama_lengkap: String,
+    val jenis_kelamin: String,
+    val tanggal_lahir: String,
+    val kewarganegaraan: String,
+    val posisi: String,
+    val nomor_passport: String,
+    val kontak: String,
+    val alamat: String
+)
