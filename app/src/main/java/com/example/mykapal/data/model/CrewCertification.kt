71203913da138077b782534c923e232a7b3581ce
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2026-01-16 03:23:21 +0700

    Menambahkan model CrewCertification untuk relasi kru dan sertifikasi

---
+++ app/src/main/java/com/example/mykapal/data/model/CrewCertification.kt
@@ -0,0 +1,12 @@
+package com.example.mykapal.data.model
+
+data class CrewCertification(
+    val crew_cert_id: Int = 0,
+    val crew_id: Int = 0,
+    val certification_id: Int = 0,
+    val nama_sertifikasi: String = "",
+    val kru: String = "",
+    val tanggal_terbit: String = "",
+    val tanggal_kadaluarsa: String = "",
+    val status: String = ""
+)
